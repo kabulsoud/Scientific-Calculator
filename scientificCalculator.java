b4145dea815b7e6b6bef20d5e7fc70da69165783
kabulsoud <kareemabulsoud96@gmail.com>
2023-06-05 16:11:09 -0400

    Setting up the main class for calculator

---
+++ scientificCalculator.java
@@ -0,0 +1,8 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class scientificCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
